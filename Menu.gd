e0c7019709025c9accd6116284581f65f9aa2f93
Cyrus Yip <cyruscmyip1@gmail.com>
2021-03-25 08:17:05 -0700

    button click

--- Menu.gd
+++ Menu.gd
@@ -1,7 +1,12 @@
 extends CanvasLayer
 
 func _ready():
+	$Fade/Quit.connect("pressed", self, "_on_quit")
 	$Fade/Retry.connect("pressed", self, "_on_retry")
+
+func _on_quit():
+	GlobalClick.play()
 
 func _on_retry():
 	get_tree().reload_current_scene()
+	GlobalClick.play()
